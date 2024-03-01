fix: change calculation of MC-Sampler (broken, total count increasing)

spin-onehalf-square-hcbosons/hamiltonian.py
@@ -35,6 +35,7 @@ class Hamiltonian(ABC):
         self,
         time: float,
         system_state_object: state.SystemState,
+        system_state_array: np.ndarray,
     ) -> float:
         pass
 
@@ -43,6 +44,7 @@ class Hamiltonian(ABC):
         self,
         time: float,
         system_state_object: state.SystemState,
+        system_state_array: np.ndarray,
     ) -> float:
         pass
 
@@ -98,10 +100,11 @@ class HardcoreBosonicHamiltonian(Hamiltonian):
         self,
         time: float,
         system_state_object: state.SystemState,
+        system_state_array: np.ndarray,
     ) -> np.complex128:
-        system_state_array = system_state_object.get_state_array()
         operator_evaluations = self.V_parts(
             system_state_object=system_state_object,
+            system_state_array=system_state_array,
         )
 
         psi_N = system_state_object.get_Psi_of_N(system_state_array=system_state_array)
@@ -234,14 +237,12 @@ class HardcoreBosonicHamiltonian(Hamiltonian):
         return total_sum * self.J
 
     def V_parts(
-        self,
-        system_state_object: state.SystemState,
+        self, system_state_object: state.SystemState, system_state_array: np.ndarray
     ) -> Dict[VPartsMapping, List[Tuple[int, int, np.ndarray]]]:
         """
         returns Tuple[l,m,K] The neighbor summation indices l&m and the resulting state K where a match was made
         """
         number_sites = system_state_object.get_number_sites_wo_spin_degree()
-        system_state_array = system_state_object.get_state_array()
 
         result: Dict[VPartsMapping, List[Tuple[int, int, np.ndarray]]] = {}
         for val in VPartsMapping:
@@ -356,13 +357,15 @@ class HardcoreBosonicHamiltonian(Hamiltonian):
         self,
         time: float,
         system_state_object: state.SystemState,
+        system_state_array: np.ndarray,
     ) -> float:
         H_n = self.get_H_n(
             time=time,
             system_state_object=system_state_object,
+            system_state_array=system_state_array,
         )
         E_zero_n = self.get_base_energy(
             system_state_object=system_state_object,
-            system_state_array=system_state_object.get_state_array(),
+            system_state_array=system_state_array,
         )
         return np.exp(H_n - (1j * E_zero_n * time))

spin-onehalf-square-hcbosons/sampler.py
@@ -14,7 +14,7 @@ class GeneralSampler(ABC):
         self.system_state = system_state
 
     @abstractmethod
-    def sample_generator(self) -> Generator[state.SystemState, None, None]:
+    def sample_generator(self, time: float) -> Generator[state.SystemState, None, None]:
         """
         Generator of system states. Call next(gen) to get more SystemStates
         """
@@ -31,14 +31,9 @@ class GeneralSampler(ABC):
 
 
 class MonteCarloSampler(GeneralSampler):
-    """
-    beta is basically 1/(k_B*T) and should have the same scale as the energy function
-    """
-
     def __init__(
         self,
         system_state: state.SystemState,
-        beta: float,
         system_hamiltonian: hamiltonian.Hamiltonian,
         random_generator: RandomGenerator,
         no_samples: int,
@@ -47,7 +42,6 @@ class MonteCarloSampler(GeneralSampler):
         no_thermalization_steps: int,
         initial_fill_level: float,
     ):
-        self.beta = beta
         self.system_hamiltonian = system_hamiltonian
         self.random_generator = random_generator
         self.no_samples = no_samples
@@ -55,11 +49,10 @@ class MonteCarloSampler(GeneralSampler):
         self.no_random_flips = no_random_flips
         self.no_thermalization_steps = no_thermalization_steps
         self.thermalized = False
-        self.fill_level_initialized = False
         self.initial_fill_level = initial_fill_level
         super().__init__(system_state=system_state)
 
-    def do_metropolis_steps(self, num_steps: int) -> None:
+    def do_metropolis_steps(self, num_steps: int, time: float) -> None:
         """
         Advances the system_state in-place by num_steps metropolis steps
         """
@@ -70,13 +63,26 @@ class MonteCarloSampler(GeneralSampler):
                 no_flips=self.no_random_flips, random_generator=self.random_generator
             )
 
-            # Calculate the energies
-            original_state_energy = self.system_hamiltonian.get_base_energy(
-                self.system_state, original_state_array
+            # Calculate the energies and probabilities
+            original_state_psi = self.system_state.get_Psi_of_N(
+                system_state_array=original_state_array
             )
-
-            proposed_state_energy = self.system_hamiltonian.get_base_energy(
-                self.system_state, proposed_state_array
+            original_state_energy_exp = (
+                self.system_hamiltonian.get_exp_H_effective_of_n_and_t(
+                    system_state_object=self.system_state,
+                    system_state_array=original_state_array,
+                    time=time,
+                )
+            )
+            proposed_state_psi = self.system_state.get_Psi_of_N(
+                system_state_array=proposed_state_array
+            )
+            proposed_state_energy_exp = (
+                self.system_hamiltonian.get_exp_H_effective_of_n_and_t(
+                    system_state_object=self.system_state,
+                    system_state_array=proposed_state_array,
+                    time=time,
+                )
             )
 
             # Calculate the acceptance ratio
@@ -87,7 +93,11 @@ class MonteCarloSampler(GeneralSampler):
             # if proposed_state_energy (final) <= original_state_energy (initial), then
             # exp (-negative) > 1 -> 1 is taken
             acceptance_ratio = min(
-                1, np.exp(-self.beta * (proposed_state_energy - original_state_energy))
+                1,
+                (
+                    (proposed_state_psi * proposed_state_energy_exp)
+                    / (original_state_psi * original_state_energy_exp)
+                ),
             )
 
             # Accept or reject the proposed state
@@ -95,43 +105,38 @@ class MonteCarloSampler(GeneralSampler):
                 self.system_state.set_state(proposed_state_array)
 
     def initialize_fill_level(self):
-        if not self.fill_level_initialized:
-            self.system_state.init_random_filling(
-                fill_ratio=self.initial_fill_level,
-                random_generator=self.random_generator,
-            )
+        self.system_state.init_random_filling(
+            fill_ratio=self.initial_fill_level,
+            random_generator=self.random_generator,
+        )
+        self.thermalized = False
 
-            self.fill_level_initialized = True
-
-    def thermalize(self):
+    def thermalize(self, time: float):
         if not self.thermalized:
-            self.do_metropolis_steps(self.no_thermalization_steps)
+            self.do_metropolis_steps(self.no_thermalization_steps, time=time)
             self.thermalized = True
 
-    def sample_generator(self) -> Generator[state.SystemState, None, None]:
+    def sample_generator(self, time: float) -> Generator[state.SystemState, None, None]:
         self.initialize_fill_level()
-        self.thermalize()  # make sure, to thermalize the system at least once
+        self.thermalize(time)  # make sure, to thermalize the system at least once
 
         for _ in range(self.no_samples):
             yield self.system_state
-            self.do_metropolis_steps(self.no_intermediate_mc_steps)
+            self.do_metropolis_steps(self.no_intermediate_mc_steps, time=time)
 
     def produces_samples_count(self) -> int:
         return self.no_samples
 
 
 class ExactSampler(GeneralSampler):
-    """
-    beta is basically 1/(k_B*T) and should have the same scale as the energy function
-    """
-
     def __init__(
         self,
         system_state: state.SystemState,
     ):
         super().__init__(system_state=system_state)
 
-    def sample_generator(self) -> Generator[state.SystemState, None, None]:
+    def sample_generator(self, time: float) -> Generator[state.SystemState, None, None]:
+        time  # this generator is independent of time
         working_state = np.zeros_like(self.system_state.get_state_array())
         self.system_state.set_state(working_state)
 