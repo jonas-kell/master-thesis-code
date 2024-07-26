94ce6111b001b3b1bcaceedc9dbc8ff0f8097292
Jonas Kell <jonas-kell@web.de>
2024-07-26 13:24:53 +0200

    feat: pure logic implementation with ifs

--- computation-scripts/analyticallogicalfunctions.py
+++ computation-scripts/analyticallogicalfunctions.py
@@ -56,3 +56,111 @@ def part_C_flipping(
     res -= Ldp and not Mdp and Mup and not Lup
 
     return res
+
+
+def part_A_flipping_if(
+    Lu: int, Ld: int, Mu: int, Md: int, flip_up: bool, flip_l: bool
+) -> int:
+
+    if flip_up:
+        if flip_l:
+            Lup = not Lu
+            Ldp = Ld
+            Mup = Mu
+            Mdp = Md
+        else:
+            Lup = Lu
+            Ldp = Ld
+            Mup = not Mu
+            Mdp = Md
+    else:
+        if flip_l:
+            Lup = Lu
+            Ldp = not Ld
+            Mup = Mu
+            Mdp = Md
+        else:
+            Lup = Lu
+            Ldp = Ld
+            Mup = Mu
+            Mdp = not Md
+
+    res = 0
+    res += Lu and not Mu and Ld == Md
+    res -= Lup and not Mup and Ldp == Mdp
+    res += Ld and not Md and Lu == Mu
+    res -= Ldp and not Mdp and Lup == Mup
+
+    return res
+
+
+def part_B_flipping_if(
+    Lu: int, Ld: int, Mu: int, Md: int, flip_up: bool, flip_l: bool
+) -> int:
+
+    if flip_up:
+        if flip_l:
+            Lup = not Lu
+            Ldp = Ld
+            Mup = Mu
+            Mdp = Md
+        else:
+            Lup = Lu
+            Ldp = Ld
+            Mup = not Mu
+            Mdp = Md
+    else:
+        if flip_l:
+            Lup = Lu
+            Ldp = not Ld
+            Mup = Mu
+            Mdp = Md
+        else:
+            Lup = Lu
+            Ldp = Ld
+            Mup = Mu
+            Mdp = not Md
+
+    res = 0
+    res += Lu and not Mu and not Md and Ld
+    res -= Lup and not Mup and not Mdp and Ldp
+    res += Ld and not Md and not Mu and Lu
+    res -= Ldp and not Mdp and not Mup and Lup
+
+    return res
+
+
+def part_C_flipping_if(
+    Lu: int, Ld: int, Mu: int, Md: int, flip_up: bool, flip_l: bool
+) -> int:
+
+    if flip_up:
+        if flip_l:
+            Lup = not Lu
+            Ldp = Ld
+            Mup = Mu
+            Mdp = Md
+        else:
+            Lup = Lu
+            Ldp = Ld
+            Mup = not Mu
+            Mdp = Md
+    else:
+        if flip_l:
+            Lup = Lu
+            Ldp = not Ld
+            Mup = Mu
+            Mdp = Md
+        else:
+            Lup = Lu
+            Ldp = Ld
+            Mup = Mu
+            Mdp = not Md
+
+    res = 0
+    res += Lu and not Mu and Md and not Ld
+    res -= Lup and not Mup and Mdp and not Ldp
+    res += Ld and not Md and Mu and not Lu
+    res -= Ldp and not Mdp and Mup and not Lup
+
+    return res
--- computation-scripts/speedcomparisonfunctions.py
+++ computation-scripts/speedcomparisonfunctions.py
@@ -3,6 +3,9 @@ from analyticallogicalfunctions import (
     part_A_flipping as flipping_A_pure_logic,
     part_B_flipping as flipping_B_pure_logic,
     part_C_flipping as flipping_C_pure_logic,
+    part_A_flipping_if as flipping_A_pure_logic_if,
+    part_B_flipping_if as flipping_B_pure_logic_if,
+    part_C_flipping_if as flipping_C_pure_logic_if,
 )
 from logicalcalcfunctions import (
     part_A_flipping as flipping_A_espresso,
@@ -14,23 +17,36 @@ from random import randrange
 
 # ! validity check
 start = time.time()
-for i in range(1000):
+for i in range(200000):
     a = randrange(2)
     b = randrange(2)
     c = randrange(2)
     d = randrange(2)
     e = randrange(2) == 1
     f = randrange(2) == 1
-    if flipping_A_pure_logic(a, b, c, d, e, f) != flipping_A_espresso(a, b, c, d, e, f):
+    if flipping_A_pure_logic(a, b, c, d, e, f) != flipping_A_espresso(
+        a, b, c, d, e, f
+    ) or flipping_A_espresso(a, b, c, d, e, f) != flipping_A_pure_logic_if(
+        a, b, c, d, e, f
+    ):
         print("alarm: A")
         print(a, b, c, d, e, f)
         exit()
-    if flipping_B_pure_logic(a, b, c, d, e, f) != flipping_B_espresso(a, b, c, d, e, f):
+    if flipping_B_pure_logic(a, b, c, d, e, f) != flipping_B_espresso(
+        a, b, c, d, e, f
+    ) or flipping_B_espresso(a, b, c, d, e, f) != flipping_B_pure_logic_if(
+        a, b, c, d, e, f
+    ):
         print("alarm: B")
         print(a, b, c, d, e, f)
         exit()
-    if flipping_C_pure_logic(a, b, c, d, e, f) != flipping_C_espresso(a, b, c, d, e, f):
+    if flipping_C_pure_logic(a, b, c, d, e, f) != flipping_C_espresso(
+        a, b, c, d, e, f
+    ) or flipping_C_espresso(a, b, c, d, e, f) != flipping_C_pure_logic_if(
+        a, b, c, d, e, f
+    ):
         print("alarm: C")
         print(a, b, c, d, e, f)
+        exit()
 end = time.time()
 print(f"took {end-start:.4}s")
