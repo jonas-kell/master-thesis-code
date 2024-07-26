58feb9be4b3275cb7e17a258ce6ab1888703d219
Jonas Kell <jonas-kell@web.de>
2024-07-26 14:10:06 +0200

    feat: hopping implementation of most optimized function type

--- computation-scripts/analyticallogicalfunctions.py
+++ computation-scripts/analyticallogicalfunctions.py
@@ -5,11 +5,13 @@ def part_A_flipping_if_free(
     Lu: int, Ld: int, Mu: int, Md: int, flip_up: bool, flip_l: bool
 ) -> int:
 
+    # ! make sure to clone block to all the same functions, inline for speed
     # controlled negation of one of the one var decided by flip_up/flip_l, purely computational, no jumps (probably slower in python)
     Lup = Lu == (not (flip_up and flip_l))
     Mup = Mu == (not flip_up or flip_l)  # (not (flip_up and not flip_l))
     Ldp = Ld == (flip_up or not flip_l)  # (not (not flip_up and flip_l))
     Mdp = Md == (flip_up or flip_l)  # (not (not flip_up and not flip_l))
+    # ! make sure to clone block to all the same functions, inline for speed
 
     res = 0
     res += Lu and not Mu and Ld == Md
@@ -24,11 +26,13 @@ def part_B_flipping_if_free(
     Lu: int, Ld: int, Mu: int, Md: int, flip_up: bool, flip_l: bool
 ) -> int:
 
+    # ! make sure to clone block to all the same functions, inline for speed
     # controlled negation of one of the one var decided by flip_up/flip_l, purely computational, no jumps (probably slower in python)
     Lup = Lu == (not (flip_up and flip_l))
     Mup = Mu == (not flip_up or flip_l)  # (not (flip_up and not flip_l))
     Ldp = Ld == (flip_up or not flip_l)  # (not (not flip_up and flip_l))
     Mdp = Md == (flip_up or flip_l)  # (not (not flip_up and not flip_l))
+    # ! make sure to clone block to all the same functions, inline for speed
 
     res = 0
     res += Lu and not Mu and not Md and Ld
@@ -43,11 +47,13 @@ def part_C_flipping_if_free(
     Lu: int, Ld: int, Mu: int, Md: int, flip_up: bool, flip_l: bool
 ) -> int:
 
+    # ! make sure to clone block to all the same functions, inline for speed
     # controlled negation of one of the one var decided by flip_up/flip_l, purely computational, no jumps (probably slower in python)
     Lup = Lu == (not (flip_up and flip_l))
     Mup = Mu == (not flip_up or flip_l)  # (not (flip_up and not flip_l))
     Ldp = Ld == (flip_up or not flip_l)  # (not (not flip_up and flip_l))
     Mdp = Md == (flip_up or flip_l)  # (not (not flip_up and not flip_l))
+    # ! make sure to clone block to all the same functions, inline for speed
 
     res = 0
     res += Lu and not Mu and Md and not Ld
@@ -62,6 +68,7 @@ def part_A_flipping(
     Lu: int, Ld: int, Mu: int, Md: int, flip_up: bool, flip_l: bool
 ) -> int:
 
+    # ! make sure to clone block to all the same functions, inline for speed
     if flip_up:
         if flip_l:
             Lup = not Lu
@@ -84,6 +91,7 @@ def part_A_flipping(
             Ldp = Ld
             Mup = Mu
             Mdp = not Md
+    # ! make sure to clone block to all the same functions, inline for speed
 
     res = 0
     res += Lu and not Mu and Ld == Md
@@ -98,6 +106,7 @@ def part_B_flipping(
     Lu: int, Ld: int, Mu: int, Md: int, flip_up: bool, flip_l: bool
 ) -> int:
 
+    # ! make sure to clone block to all the same functions, inline for speed
     if flip_up:
         if flip_l:
             Lup = not Lu
@@ -120,6 +129,7 @@ def part_B_flipping(
             Ldp = Ld
             Mup = Mu
             Mdp = not Md
+    # ! make sure to clone block to all the same functions, inline for speed
 
     res = 0
     res += Lu and not Mu and not Md and Ld
@@ -134,6 +144,7 @@ def part_C_flipping(
     Lu: int, Ld: int, Mu: int, Md: int, flip_up: bool, flip_l: bool
 ) -> int:
 
+    # ! make sure to clone block to all the same functions, inline for speed
     if flip_up:
         if flip_l:
             Lup = not Lu
@@ -156,6 +167,199 @@ def part_C_flipping(
             Ldp = Ld
             Mup = Mu
             Mdp = not Md
+    # ! make sure to clone block to all the same functions, inline for speed
+
+    res = 0
+    res += Lu and not Mu and Md and not Ld
+    res -= Lup and not Mup and Mdp and not Ldp
+    res += Ld and not Md and Mu and not Lu
+    res -= Ldp and not Mdp and Mup and not Lup
+
+    return res
+
+
+def part_A_hopping(
+    Lu: int,
+    Ld: int,
+    Mu: int,
+    Md: int,
+    I: int,
+    swp_i_l: bool | None,
+    i_up: bool,
+    J: int,
+    j_up: bool,
+    swp_j_l: bool | None,
+) -> int:
+
+    # ! make sure to clone block to all the same functions, inline for speed
+    if swp_i_l is not None:
+        if swp_i_l:
+            if i_up:
+                Lup = I
+                Ldp = Ld
+                Mup = Mu
+                Mdp = Md
+            else:
+                Lup = Lu
+                Ldp = I
+                Mup = Mu
+                Mdp = Md
+        else:
+            if i_up:
+                Lup = Lu
+                Ldp = Ld
+                Mup = I
+                Mdp = Md
+            else:
+                Lup = Lu
+                Ldp = Ld
+                Mup = Mu
+                Mdp = I
+    else:
+        Lup = Lu
+        Ldp = Ld
+        Mup = Mu
+        Mdp = I
+    if swp_j_l is not None:
+        if swp_j_l:
+            if j_up:
+                Lup = J
+            else:
+                Ldp = J
+        else:
+            if j_up:
+                Mup = J
+            else:
+                Mdp = J
+    # ! make sure to clone block to all the same functions, inline for speed
+
+    res = 0
+    res += Lu and not Mu and Ld == Md
+    res -= Lup and not Mup and Ldp == Mdp
+    res += Ld and not Md and Lu == Mu
+    res -= Ldp and not Mdp and Lup == Mup
+
+    return res
+
+
+def part_B_hopping(
+    Lu: int,
+    Ld: int,
+    Mu: int,
+    Md: int,
+    I: int,
+    swp_i_l: bool | None,
+    i_up: bool,
+    J: int,
+    j_up: bool,
+    swp_j_l: bool | None,
+) -> int:
+
+    # ! make sure to clone block to all the same functions, inline for speed
+    if swp_i_l is not None:
+        if swp_i_l:
+            if i_up:
+                Lup = I
+                Ldp = Ld
+                Mup = Mu
+                Mdp = Md
+            else:
+                Lup = Lu
+                Ldp = I
+                Mup = Mu
+                Mdp = Md
+        else:
+            if i_up:
+                Lup = Lu
+                Ldp = Ld
+                Mup = I
+                Mdp = Md
+            else:
+                Lup = Lu
+                Ldp = Ld
+                Mup = Mu
+                Mdp = I
+    else:
+        Lup = Lu
+        Ldp = Ld
+        Mup = Mu
+        Mdp = I
+    if swp_j_l is not None:
+        if swp_j_l:
+            if j_up:
+                Lup = J
+            else:
+                Ldp = J
+        else:
+            if j_up:
+                Mup = J
+            else:
+                Mdp = J
+    # ! make sure to clone block to all the same functions, inline for speed
+
+    res = 0
+    res += Lu and not Mu and not Md and Ld
+    res -= Lup and not Mup and not Mdp and Ldp
+    res += Ld and not Md and not Mu and Lu
+    res -= Ldp and not Mdp and not Mup and Lup
+
+    return res
+
+
+def part_C_hopping(
+    Lu: int,
+    Ld: int,
+    Mu: int,
+    Md: int,
+    I: int,
+    swp_i_l: bool | None,
+    i_up: bool,
+    J: int,
+    j_up: bool,
+    swp_j_l: bool | None,
+) -> int:
+
+    # ! make sure to clone block to all the same functions, inline for speed
+    if swp_i_l is not None:
+        if swp_i_l:
+            if i_up:
+                Lup = I
+                Ldp = Ld
+                Mup = Mu
+                Mdp = Md
+            else:
+                Lup = Lu
+                Ldp = I
+                Mup = Mu
+                Mdp = Md
+        else:
+            if i_up:
+                Lup = Lu
+                Ldp = Ld
+                Mup = I
+                Mdp = Md
+            else:
+                Lup = Lu
+                Ldp = Ld
+                Mup = Mu
+                Mdp = I
+    else:
+        Lup = Lu
+        Ldp = Ld
+        Mup = Mu
+        Mdp = I
+    if swp_j_l is not None:
+        if swp_j_l:
+            if j_up:
+                Lup = J
+            else:
+                Ldp = J
+        else:
+            if j_up:
+                Mup = J
+            else:
+                Mdp = J
+    # ! make sure to clone block to all the same functions, inline for speed
 
     res = 0
     res += Lu and not Mu and Md and not Ld
