fix: correct calculation of spin observables

calculation-helpers/concurrence-tests/combineSpinBasisValues.py
@@ -108,7 +108,7 @@ densityMatrixBuilderArray = np.array(
             ],
         ],
     ],
-    dtype=np.complex64,
+    dtype=np.complex128,
 )
 
 

calculation-helpers/concurrence-tests/occMeasurementsToSpinBasis.py
@@ -31,7 +31,7 @@ def matrix_obvious(a, b, c, d, e, f, g, h) -> np.ndarray:
                     4 * h - b - d + 1,
                 ],
             ],
-            dtype=np.complex64,
+            dtype=np.complex128,
         )
         / 4
     )
@@ -57,7 +57,7 @@ factor_to_spin_basis_conversion_helper = (
             [-1j, 0, -2j, 2j, 2j, 4j],  # zy
             [3, 1, 1, -4, -4, -4],  # zz
         ],
-        dtype=np.complex64,
+        dtype=np.complex128,
     )
     / 4
 )

calculation-helpers/concurrence-tests/spinToZBasisTransformation.py
@@ -24,7 +24,7 @@ def getMatrixInZBasis(
 ) -> np.ndarray:
     indexTranslationMap = {"uu": 0, "ud": 1, "du": 2, "dd": 3}
 
-    res = np.zeros((4, 4), dtype=np.complex64)
+    res = np.zeros((4, 4), dtype=np.complex128)
 
     for summandOfA in sva:
         plusA, has_iA, udketA, udbraA = summandOfA

computation-scripts/concurrence.py
@@ -108,7 +108,7 @@ density_matrix_builder_helper = np.array(
             ],
         ],
     ],
-    dtype=np.complex64,
+    dtype=np.complex128,
 )
 
 
@@ -118,51 +118,6 @@ def spin_basis_to_z_basis(values: np.ndarray) -> np.ndarray:
         density_matrix_builder_helper * values[:, :, np.newaxis, np.newaxis],
         axis=(0, 1),
     )
-
-
-measurement_to_spin_basis_conversion_helper = (
-    np.array(
-        [  # 1  a  c  e  f  g
-            [1, 0, 0, 0, 0, 0],  # 00 == 1
-            [1, 0, 0, 0, 0, 0],  # 0x == 1
-            [-1j, 0, 2j, 0, 0, 0],  # 0y
-            [1, 0, -2, 0, 0, 0],  # 0z
-            [1, 0, 0, 0, 0, 0],  # x0 == 1
-            [1, 0, 0, 0, 0, 0],  # xx == 1
-            [-1j, 0, 0, 2j, 0, 2j],  # xy
-            [1, 0, 0, -2, 0, -2],  # xz
-            [-1j, 2j, 0, 0, 0, 0],  # y0
-            [-1j, 0, 0, 2j, 2j, 0],  # yx
-            [-1, 0, 0, 0, 2, 2],  # yy
-            [-1j, -2j, 0, 2j, 4j, 2j],  # yz
-            [1, -2, 0, 0, 0, 0],  # z0
-            [1, 0, 0, -2, -2, 0],  # zx
-            [-1j, 0, -2j, 2j, 2j, 4j],  # zy
-            [3, 1, 1, -4, -4, -4],  # zz
-        ],
-        dtype=np.complex64,
-    )
-    / 4
-)
-
-
-def measurements_to_spin_basis(acefg: np.ndarray) -> np.ndarray:
-    """acefg are the averaged measurements of observables:
-
-    a: c_l,sigma (to left: (1-n_l,sigma) )
-
-    c: c_m,sigma' (to left: (1-n_m,sigma') )
-
-    e: c_l,sigma*c_m,sigma' (to left: ((1-n_l,sigma) * (1-n_m,sigma')) )
-
-    f: c_l,sigma*c#_m,sigma' (to left: ((1-n_l,sigma) * n_m,sigma') )
-
-    g: c#_l,sigma*c_m,sigma' (to left: (n_l,sigma * (1-n_m,sigma')) )
-    """
-
-    oacefg = np.insert(acefg, 0, 1)
-
-    return (measurement_to_spin_basis_conversion_helper @ oacefg).reshape((4, 4))
 
 
 sigma_y_sigma_y = np.array(
@@ -210,32 +165,21 @@ def trace_is_one(test: np.ndarray) -> bool:
     return np.abs(purity - 1) < 1e-4
 
 
-def calculate_concurrence(acefg: np.ndarray, do_checks: bool = False) -> float:
-    """acefg are the averaged measurements of observables:
-
-    a: c_l,sigma (to left: (1-n_l,sigma) )
-
-    c: c_m,sigma' (to left: (1-n_m,sigma') )
-
-    e: c_l,sigma*c_m,sigma' (to left: ((1-n_l,sigma) * (1-n_m,sigma')) )
-
-    f: c_l,sigma*c#_m,sigma' (to left: ((1-n_l,sigma) * n_m,sigma') )
-
-    g: c#_l,sigma*c_m,sigma' (to left: (n_l,sigma * (1-n_m,sigma')) )
-    """
-    acefg_real = np.real(acefg)
-    imag_error = np.sum(np.abs(acefg_real - acefg))
+def calculate_concurrence(
+    spin_basis_measurements: np.ndarray, do_checks: bool = False
+) -> float:
+    spin_basis_measurements_real = np.real(spin_basis_measurements)
+    imag_error = np.sum(np.abs(spin_basis_measurements_real - spin_basis_measurements))
     if imag_error > 1e-4:
         if do_checks:
-            print(acefg)
+            print(spin_basis_measurements)
             raise Exception("A complex part in the measurements was ommitted")
         else:
             print(
                 f"Warning intermediate observables had imaginary part of {imag_error:.6f} that was omitted"
             )
 
-    spin_basis_values = measurements_to_spin_basis(acefg_real)
-    z_basis_values = spin_basis_to_z_basis(spin_basis_values)
+    z_basis_values = spin_basis_to_z_basis(spin_basis_measurements / 4.0)
     if do_checks:
         if not is_hermitian(z_basis_values):
             print(z_basis_values)

computation-scripts/observables.py
@@ -217,6 +217,11 @@ class Concurrence(Observable):
         self.name_from = str(site_index_from) + (" up" if spin_up_from else " down")
         self.name_to = str(site_index_to) + (" up" if spin_up_to else " down")
 
+        self.index_l = site_index_from
+        self.up_sigma = spin_up_from
+        self.index_m = site_index_to
+        self.up_sigma_prime = spin_up_to
+
         self.use_index_from = site_index_from  # l
         if not spin_up_from:
             self.use_index_from = system_geometry.get_opposite_spin_index(
@@ -235,22 +240,84 @@ class Concurrence(Observable):
     ) -> np.complex128:
         system_state_array = system_state.get_state_array()
 
-        anti_site_occ_l_sigma = 1 - system_state_array[self.use_index_from]
-        anti_site_occ_m_sigma_prime = 1 - system_state_array[self.use_index_to]
+        site_occ_l_sigma_sign = 2 * system_state_array[self.use_index_from] - 1
+        site_occ_m_sigma_prime_sign = 2 * system_state_array[self.use_index_to] - 1
+        # notice minus for e^H_eff_tilde-H_eff factors: difference is wrong way round from function. need (flipped - original)
+        e_to_diff_l_sigma = np.eps(
+            -self.system_hamiltonian.get_H_eff_difference_flipping(
+                time=time,
+                flipping_up=self.up_sigma,
+                flipping_index=self.index_l,
+                before_swap_system_state=system_state,
+            )
+        )
+        e_to_diff_m_sigma_prime = np.eps(
+            -self.system_hamiltonian.get_H_eff_difference_flipping(
+                time=time,
+                flipping_up=self.up_sigma_prime,
+                flipping_index=self.index_m,
+                before_swap_system_state=system_state,
+            )
+        )
+        e_to_diff_both = np.eps(
+            -self.system_hamiltonian.get_H_eff_difference_double_flipping(
+                time=time,
+                flipping1_up=self.up_sigma,
+                flipping1_index=self.index_l,
+                flipping2_up=self.up_sigma_prime,
+                flipping2_index=self.index_m,
+                before_swap_system_state=system_state,
+            )
+        )
 
-        # a: c_l,sigma (to left: (1-n_l,sigma) )
-        # c: c_m,sigma' (to left: (1-n_m,sigma') )
-        # e: c_l,sigma*c_m,sigma' (to left: ((1-n_l,sigma) * (1-n_m,sigma')) )
-        # f: c_l,sigma*c#_m,sigma' (to left: ((1-n_l,sigma) * n_m,sigma') )
-        # g: c#_l,sigma*c_m,sigma' (to left: (n_l,sigma * (1-n_m,sigma')) )
         return np.array(
             [
-                anti_site_occ_l_sigma,
-                anti_site_occ_m_sigma_prime,
-                anti_site_occ_l_sigma * anti_site_occ_m_sigma_prime,
-                anti_site_occ_l_sigma * (1 - anti_site_occ_m_sigma_prime),
-                (1 - anti_site_occ_l_sigma) * anti_site_occ_m_sigma_prime,
-            ]
+                [
+                    1,  # 0(l,sigma) 0(m,sigma_p)
+                    e_to_diff_m_sigma_prime,  # 0(l,sigma) x(m,sigma_p)
+                    e_to_diff_m_sigma_prime
+                    * -1j
+                    * site_occ_m_sigma_prime_sign,  # 0(l,sigma) y(m,sigma_p)
+                    site_occ_m_sigma_prime_sign,  # 0(l,sigma) z(m,sigma_p)
+                ],
+                [
+                    e_to_diff_l_sigma,  # x(l,sigma) 0(m,sigma_p)
+                    e_to_diff_both,  # x(l,sigma) x(m,sigma_p)
+                    e_to_diff_both
+                    * site_occ_m_sigma_prime_sign
+                    * 1j,  # x(l,sigma) y(m,sigma_p)
+                    e_to_diff_l_sigma
+                    * site_occ_m_sigma_prime_sign,  # x(l,sigma) z(m,sigma_p)
+                ],
+                [
+                    e_to_diff_l_sigma
+                    * -1j
+                    * site_occ_l_sigma_sign,  # y(l,sigma) 0(m,sigma_p)
+                    e_to_diff_both
+                    * site_occ_l_sigma_sign
+                    * 1j,  # y(l,sigma) x(m,sigma_p)
+                    e_to_diff_both
+                    * (
+                        -1 * site_occ_l_sigma_sign * site_occ_m_sigma_prime_sign
+                    ),  # y(l,sigma) y(m,sigma_p)
+                    e_to_diff_l_sigma
+                    * -1j
+                    * site_occ_l_sigma_sign
+                    * site_occ_m_sigma_prime_sign,  # y(l,sigma) z(m,sigma_p)
+                ],
+                [
+                    site_occ_l_sigma_sign,  # z(l,sigma) 0(m,sigma_p)
+                    site_occ_l_sigma_sign
+                    * e_to_diff_m_sigma_prime,  # z(l,sigma) x(m,sigma_p)
+                    site_occ_l_sigma_sign
+                    * e_to_diff_m_sigma_prime
+                    * -1j
+                    * site_occ_m_sigma_prime_sign,  # z(l,sigma) y(m,sigma_p)
+                    site_occ_l_sigma_sign
+                    * site_occ_m_sigma_prime_sign,  # z(l,sigma) z(m,sigma_p)
+                ],
+            ],
+            dtype=np.complex128,
         )
 
     def post_process_necessary(self) -> bool: